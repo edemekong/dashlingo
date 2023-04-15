4b038fef882d4bb23dcf221dc493528b49167b93
Paul Edemekong <hello.paulvin@gmail.com>
2023-04-15 01:56:25 +0100

    added tool tip ui

---
+++ lib/constants/durations.dart
@@ -0,0 +1,7 @@
+class AppDurations {
+  AppDurations._();
+
+  static const Duration fast = Duration(milliseconds: 200);
+  static const Duration midSpeed = Duration(milliseconds: 350);
+  static const Duration slow = Duration(milliseconds: 400);
+}
